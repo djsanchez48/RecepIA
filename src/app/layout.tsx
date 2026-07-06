d8ebcd1bd862942e6f2d40c50c72b85c41017fbd
djsanchez48 <djsanchez48.ds@gmail.com>
2026-07-05 21:05:32 -0500

    Add suppressHydrationWarning to body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col pb-16">
+      <body className="min-h-full flex flex-col pb-16" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
